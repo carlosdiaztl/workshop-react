42208987091338c13d171d45d87483f5da44d209
carlos diaz <108467585+carlosdiaztl@users.noreply.github.com>
2022-10-10 16:38:36 -0500

    create new branch

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Footer from './footer/footer.jsx';
+import Header from './header/Header.jsx';
 
 class App extends Component {
 
     render(){
         return (
             <main >
-            <Footer/>
+            <Header/>
 
                 
             </main>
