1fb8ad952c0d13b576a53d8ae08a2e475e0d2488
carlos diaz <108467585+carlosdiaztl@users.noreply.github.com>
2022-10-10 17:01:46 -0500

    sobrreescirbri

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Footer from './footer/footer.jsx';
 import Header from './header/Header.jsx';
-=======
-import Footer from './footer/Footer.jsx';
-import Main from './main/Main.jsx';
->>>>>>> 7545c9e12a227ef8587b5d1eb993b4ebbc34fa16
 
 class App extends Component {
 
     render() {
         return (
-<<<<<<< HEAD
-            <main >
-            <Header/>
-=======
             <>
-                <Main />
-                <Footer />
+
+            <Footer/>
+            <Header/>
             </>
->>>>>>> 7545c9e12a227ef8587b5d1eb993b4ebbc34fa16
 
         )
     }
